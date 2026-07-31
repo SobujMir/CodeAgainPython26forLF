668e4839491084d57182bed9765e7e7bd5a980e7
Sobuj Mir <Sobuj15-10699@diu.edu.bd>
2026-07-31 17:14:16 +0600

    My Second Change for testing

--- FirstPcode.py
+++ FirstPcode.py
@@ -1,76 +1,79 @@
-"""This is my first Python program."""
+#"""This is my first Python program."""
 # This is my first Python program
-print(" Hello, World for Python! ")
-print(5+3)
-print("This is a simple Python Program for beginners")
-print("Auto save is working fine.")
-print("$" * 10)
-print ("Hello, world missed parenthesis")
+#print(" Hello, World for Python! ")
+# print(5+3)
+# print("This is a simple Python Program for beginners")
+# print("Auto save is working fine.")
+# print("$" * 10)
+# print ("Hello, world missed parenthesis")
 
-X = 1
-Y = 2
-Sum = X + Y
+# X = 1
+# Y = 2
+# Sum = X + Y
 
-Sum = 30
+# Sum = 30
 
-print(Sum)
-Result = Sum + 10
+# print(Sum)
+# Result = Sum + 10
 
-print(Result)
+# print(Result)
 
-print("Auto save is not working fine.\n\n")
+# print("Auto save is not working fine.\n\n")
 
-print("Python run keboard shortcut is working fine.")
-print("Python run File")
+# print("Python run keboard shortcut is working fine.")
+# print("Python run File")
 
 
-# I am starting fundemental of Python programming
-Student_count = 1000
-print(Student_count)
-print(type(Student_count))
-rating = 4.5
-print(rating)
-is_published = True
-print(is_published)
-bolian_value = False
+# # I am starting fundemental of Python programming
+# # Student_count = 1000
+# # print(Student_count)
+# # print(type(Student_count))
+# # rating = 4.5
+# # print(rating)
+# # is_published = True
+# # print(is_published)
+# # bolian_value = False
 
-print(bolian_value)
+# # print(bolian_value)
 
-Course_Name = "Python Programming"
-print(Course_Name)
+# Course_Name = "Python Programming"
+# print(Course_Name)
 
-M = str(1)
-N = int(2)
-O = float(3)
-print(M,"\n", N,"\n", O)
+# M = str(1)
+# N = int(2)
+# O = float(3)
+# print(M,"\n", N,"\n", O)
 
-print(type(M))
-print(type(N))
-print(type(O))
+# print(type(M))
+# print(type(N))
+# print(type(O))
 
-One, Two, Three = 1, 2, 3
-print(One, Two, Three)
+# One, Two, Three = 1, 2, 3
+# print(One, Two, Three)
 
-# Global variable
+# # Global variable
 
-X_Global = "Awesome"
+# X_Global = "Awesome"
 
-def myfunc():
-    X_Global = "fantastic"
-    print("Python is " + X_Global)
+# def myfunc():
+#     X_Global = "fantastic"
+#     print("Python is " + X_Global)
           
 
-myfunc()
+# myfunc()
 
-print("Python is " + X_Global)
+# print("Python is " + X_Global)
 
-X_Complex = 1j
-print(X_Complex)
+# X_Complex = 1j
+# print(X_Complex)
 
-print(type(X_Complex))
+# print(type(X_Complex))
 
-print("I need to know about git and githum is very crucial for me to get Job")
+# print("I need to know about git and githum is very crucial for me to get Job")
 
-print(" Why do I cannot maintain consistency?")
-print("I need to finish git and github within week")
+# print(" Why do I cannot maintain consistency?")
+# print("I need to finish git and github within week")
+print(" Wellcome to Consistency and Discipline")
+print("Because those are the key to success in any field of life.")
+
 
