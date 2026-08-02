16e3bb91154d072d4994e3141da19db57a6ee6ab
Sobuj Mir <Sobuj15-10699@diu.edu.bd>
2026-08-02 22:21:58 +0600

    Second file

---
+++ BasicPcode.py
@@ -0,0 +1,59 @@
+print("Welcome consistency.")
+# Start from Strings in Python
+str1 = "Python is dynamic."
+# # print(str1)
+# # print(str1[0])
+# # print(str1[0:9])
+# # print(str1[2:6])
+# # print(str1[2:])
+# print(str1 * 2)
+# print(str1 + " I love Python.")
+# print(str1[:4])
+# print(str1[-1])
+# print(str1[-3:])
+
+# print(str1[1:5])
+# print(str1.upper())
+# print(str1.lower())
+
+# str2 = "   All is dynamic.   "
+# print(str2.strip())
+# print(str2.replace("dynamic", "static"))
+# print(str2.replace("l","Ll"))
+# print(str2.split(" "))
+
+# String concatenation 
+
+# a = "English"
+# b ="important"
+# c = a + " is very " + b
+# print(c)
+# print(c + " "+ str1)
+# # Formatting of string in Python
+
+# age = 20
+
+# txt = "My name is Jihad, I am " + str(age) + " years old."
+# #txt1= "I am" + age + " years old."
+# print(txt)
+# txt2 = f"My name is Jihad, I am {age} years old."
+
+# print(txt2)
+# txt3 = " the price of the puduct is {age} dollars."
+# print(txt3.format(age=30))
+# txt4= f" the price of the puduct is {age} dollars."
+# print(txt4)
+# txt5 = f" the price of the puduct is {age: .1f} dollars."
+
+# print(txt5)
+
+# print(f"The price is {30 *20} dollars.")
+
+
+txt = " Jihad is a good gamer \"Because\" he play a lot "
+# # txt1 = " it's all good"
+
+# print(txt)
+# print(txt1)
+txt1 = 'it\'s \nall \\\ good'
+print(txt1)
